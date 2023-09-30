5be8a08614ed7ae2a802a273a1ce4cbb0f48d078
sanjana1194 <141471036+sanjana1194@users.noreply.github.com>
2023-09-30 16:38:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+//import Name1 from './Name1';
+import Fetch from './Fetch';
+
+function App() {
+  return (
+    <div className='App'>
+      <Fetch/>
+    </div>
+  );
+}
+
+export default App;
